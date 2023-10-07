c4eafbfc13fec5c2704e2f2d3a7243a2fda3136b
OgawaManami <vl0724fgark@yahoo.co.jp>
2023-10-07 21:45:35 +0900

    「出品する」アイコンのルーティング設定
    # What
    ・models/item.rbにテーブルとのアソシエーション記述
    ・views/items/index.html.erbに「出品する」アイコンのルーティングを設定
    
    #Why
    商品出品機能実装のため

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
   root to: "items#index"
-  resources :users, only: [:new, :create, :edit, :update]do
-end
-resources :users, only: :show
+  resources :items do
+    resources :orders, only: [:index, :create]
+  end
 end

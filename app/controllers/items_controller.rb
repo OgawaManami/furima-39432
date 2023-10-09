37d28a3fa1dde7a53ccbc4c2e90256e31e235ae0
OgawaManami <vl0724fgark@yahoo.co.jp>
2023-10-09 18:37:16 +0900

    ActiveHash導入とビューファイル修正
    # What
    ・categoryモデル/item_conditionモデル/delivery_chargeモデル/delivery_dateモデルの作成
    
    ・app/views/new.html.erb修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,12 +8,25 @@ class ItemsController < ApplicationController
   end
 
   def new
-    #@items = Item.new
+    @items = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
 
-
   private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :item_text, :category_id, :item_condition_id, :delivery_charge_id, :prefecture_id, :delivery_date_id, :item_price).merge(user_id: current_user.id)
+  end
+
 
   def move_to_index
     unless user_signed_in?
@@ -21,8 +34,5 @@ class ItemsController < ApplicationController
     end
   end
 
-  def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
-  end
-
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,42 +4,21 @@ class Item < ApplicationRecord
     belongs_to :user
     belongs_to :category
     has_one    :order
-    has_many :comments
-    has_one :shipping
-    has_one :order
+    has_many   :comments
+    has_one    :shipping
+    has_one    :order
+
+      # active_storageとのアソシエーション
     has_one_attached :image
-  
+
     # アクティブハッシュとのアソシエーション
-    #belongs_to :category
-    #belongs_to :prefecture
-    #belongs_to :item_status
-    #belongs_to :shipping_cost
-    #belongs_to :shipping_date
+    belongs_to :category
+    belongs_to :prefecture
+    belongs_to :item_condition
+    belongs_to :delivery_charge
+    belongs_to :delivery_date
   
-    # active_storageとのアソシエーション
-    # （items・active_storage_blobsテーブルを関連付け）
-    #has_one_attached :image
-  
-    #with_options presence: true do
-      #validates :user_id
-      #validates :image
-      #validates :name
-      #validates :description
-      #validates :category_id
-      #validates :item_status_id
-      #validates :shipping_cost_id
-      #validates :prefecture_id
-      #validates :shipping_date_id
-      # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
-      #validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-    #end
-  
-    # ジャンルの選択が「--」の時は保存不可
-    #with_options numericality: { other_than: 0 } do
-      #validates :category_id
-      #validates :prefecture_id
-      #validates :item_status_id
-      #validates :shipping_cost_id
-      #validates :shipping_date_id
-    #end
+  #ジャンルの選択が「---」の時は保存できないようにする
+    validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
+
   end

eba65f85d881ba467ff048e5fab09369039fc428
OgawaManami <vl0724fgark@yahoo.co.jp>
2023-10-22 18:07:20 +0900

    オーダーフォームの実装
    # What
    オーダーフォームの実装
    
    # Why
    商品購入機能実装のため

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,23 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :non_purchased_item, only: [:index, :create]
 
   def index
     @order_form = OrderForm.new
-    @item = Item.find(params[:item_id])
+  end
+
+  def new
+    @order_form = OrderForm.new
   end
 
   def create
     @order_form = OrderForm.new(order_params)
-    @item = Item.find(params[:item_id])
     if @order_form.valid?
+      pay_item
       @order_form.save
       redirect_to root_path
     else
-      render :index
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -22,4 +26,19 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_form).permit(:zip_code, :prefecture_id, :city, :street, :building, :tel).merge(user_id: current_user.id, item_id: params[:item_id])
   end
+
+  def pay_item
+    Payjp.api_key = "sk_test_1537e3a384be7b937460c79f"
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def non_purchased_item
+    @item = Item.find(params[:item_id])
+    #redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
     root to: 'items#index'
     resources :items , only: [:new, :create, :show, :edit, :destroy, :update] do
-    resources :orders, only: [:index, :create]
+    resources :orders, only: [:index, :new, :create]
   end
 end

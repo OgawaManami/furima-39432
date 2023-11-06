a8a31e588697aff08f35e77eaa2cc9ad760ebff0
OgawaManami <vl0724fgark@yahoo.co.jp>
2023-11-06 16:15:16 +0900

    プレビュー機能の実装
    # What
    プレビュー機能の実装
    
    # Why
    フリマアプリ追加実装のため

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_28_122125) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_02_130403) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -74,6 +74,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_28_122125) do
     t.datetime "updated_at", null: false
     t.index ["item_id"], name: "index_orders_on_item_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "posts", charset: "utf8", force: :cascade do |t|
+    t.string "text", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
